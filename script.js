ac659938cc30b3802c5cd3d9a9ec41c330c1c717
Akashkr1090 <95604926+Akashkr1090@users.noreply.github.com>
2022-11-19 09:25:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('background color change on click')
 
 const container = document.querySelector('.container')
 const body = document.querySelector('body')
+let color = document.getElementById("colorName")
 
 container.addEventListener('click', changeBackground)
 // container.addEventListener('mouseover', changeBackground)
@@ -12,10 +13,7 @@ function changeBackground(){
     var z = Math.floor(Math.random()*256);
 
     var bgColor = "rgb(" + x + "," + y + "," + x + ")"
+    color.innerHTML=bgColor
     console.log(bgColor)
     document.body.style.background=bgColor;
 }
-
-// random background color channged
-
-// random_bg_color();
